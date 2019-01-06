97cb27a2ab0f965bf0177b3d6511c40aedb5e848
Sharvari <sharvaridesai@hotmail.com>
2019-01-06 18:44:05 -0500

    Update CelestialObject.cs

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -11,7 +11,7 @@ namespace StarChart.Models
         [Required]
         public string Name { get; set; }
 
-        public int? OrbittedObject{ get; set; }
+        public int? OrbitedObjectId { get; set; }
 
         [NotMapped]
         public List<CelestialObject>Satellites { get; set; }
